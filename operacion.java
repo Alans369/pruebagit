b02ae3ad1f7b30277dba8f0fe54e490f8c867dca
Alan369 <cs25001@esfe.agape.edu.sv>
2025-05-22 08:46:56 -0600

    hello

---
+++ operacion.java
@@ -0,0 +1,20 @@
+public class operacion {
+
+    public int suma(int a, int b) {
+
+        if (a<0 || b<0) {
+            System.out.println("No se puede sumar numeros negativos");
+            return -1;
+        }
+        return a + b;
+    }
+
+    public double suma(double a, double b) {
+        return a + b;
+    }
+
+    public int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
